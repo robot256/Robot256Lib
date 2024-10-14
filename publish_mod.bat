7d186c3e99a935d9eca9d8128c56d1a0abe8fd1c
robot256 <robot256@GMAIL.COM>
2024-10-14 09:07:26 -0400

    Updated publishing script

--- publish_mod.bat
+++ publish_mod.bat
@@ -18,14 +18,14 @@ set Moddir=%~dp0
 set Moddir=!Moddir:~0,-1!
 echo %Moddir%
 
-echo Copying files from %Moddir% to %Modname%_%Version%...
+echo Copying files from %Moddir% to temp\%Modname%...
 cd ..
-xcopy /S /I /E %Moddir% %Modname%_%Version% /EXCLUDE:%Moddir%\publish_exclude.txt
-
+xcopy /S /I /E %Moddir% temp\%Modname% /EXCLUDE:%Moddir%\publish_exclude.txt
+cd temp
 echo Compressing %Modname%_%Version%.zip...
-7z a %Modname%_%Version%.zip %Modname%_%Version%
+7z a ..\%Modname%_%Version%.zip %Modname%
 
 echo Cleaning up temporary files...
-rd /S /Q %Modname%_%Version%
+rd /S /Q %Modname%
 
 echo Finished publishing %Modname% Version %Version%.
